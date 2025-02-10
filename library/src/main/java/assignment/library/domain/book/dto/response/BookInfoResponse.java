9add4c2f5bdbf95fcf68fceb75e5902be8b1e58a
YunSeok-Choi <dbstjr9898@naver.com>
2025-02-10 23:18:13 +0900

    :sparkles: Feat : 도서 목록 조회 캐시 적용

--- library/src/main/java/assignment/library/domain/book/dto/response/BookInfoResponse.java
+++ library/src/main/java/assignment/library/domain/book/dto/response/BookInfoResponse.java
@@ -2,9 +2,14 @@ package assignment.library.domain.book.dto.response;
 
 import assignment.library.domain.book.entity.BookStatus;
 import com.querydsl.core.annotations.QueryProjection;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookInfoResponse {
     private Long bookId;
     private String title;
@@ -16,6 +21,7 @@ public class BookInfoResponse {
     private String tag;
     private String status;
 
+    @Builder
     @QueryProjection
     public BookInfoResponse(Long bookId, String title, String author, String isbn, String publisher,
                             String publishedDate, String category, String tag, BookStatus status) {
--- library/src/main/java/assignment/library/domain/book/service/BookServiceImpl.java
+++ library/src/main/java/assignment/library/domain/book/service/BookServiceImpl.java
@@ -7,6 +7,8 @@ import assignment.library.domain.book.entity.Book;
 import assignment.library.domain.book.repository.BookCustomRepository;
 import assignment.library.domain.book.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,16 +30,19 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
+    @Cacheable(value = "bookInfo", key = "#bookId != null ? #bookId : 'defaultKey'", unless = "#result == null or #result.isEmpty()")
     public List<BookInfoResponse> getBookInfo(Long bookId) {
         return bookCustomRepository.getBookInfo(bookId);
     }
 
     @Override
+    @CacheEvict(value = "bookInfo", key = "#bookId != null ? #bookId : 'defaultKey'")
     public void updateBook(Long bookId, UpdateBookRequest updateBookRequest) {
         bookCustomRepository.updateBook(bookId, updateBookRequest);
     }
 
     @Override
+    @CacheEvict(value = "bookInfo", key = "#bookId != null ? #bookId : 'defaultKey'")
     public void deleteBook(Long bookId) {
         bookCustomRepository.deleteBook(bookId);
     }
