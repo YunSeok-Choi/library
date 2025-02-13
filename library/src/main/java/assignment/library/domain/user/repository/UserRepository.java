53f3de6d8d1e76a81652af0e80ec5fe55eee97e3
YunSeok-Choi <dbstjr9898@naver.com>
2025-02-14 02:06:49 +0900

    :white_check_mark: Test : CI/CD TEST

--- library/src/main/java/assignment/library/domain/user/repository/UserRepository.java
+++ library/src/main/java/assignment/library/domain/user/repository/UserRepository.java
@@ -4,5 +4,4 @@ import assignment.library.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
 }
