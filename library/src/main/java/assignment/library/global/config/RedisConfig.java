beff0113401d112724db1e164d3fe4d1a038289c
YunSeok-Choi <dbstjr9898@naver.com>
2025-02-14 02:28:31 +0900

    :bug: Fix : redis 연결 오류 수정

--- library/src/main/java/assignment/library/global/config/RedisConfig.java
+++ library/src/main/java/assignment/library/global/config/RedisConfig.java
@@ -20,9 +20,15 @@ import java.time.Duration;
 @EnableCaching
 public class RedisConfig {
 
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(host, port);
     }
 
     @Bean
