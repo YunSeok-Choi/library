c93f76c18f2874906c0d588a9267a402c458e798
YunSeok-Choi <dbstjr9898@naver.com>
2025-02-13 17:39:28 +0900

    :test_tube: Test : 빌드 실패 테스트

--- library/src/test/java/assignment/library/LibraryApplicationTests.java
+++ library/src/test/java/assignment/library/LibraryApplicationTests.java
@@ -1,13 +1,30 @@
 package assignment.library;
 
+import jakarta.persistence.PreUpdate;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class LibraryApplicationTests {
 
+	public int testValue;
+
+	@BeforeEach
+	void init() {
+		testValue = 1;
+	}
+
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void 테스트_확인() {
+		assertThat(testValue).isEqualTo(0);
+	}
+
 
 }
