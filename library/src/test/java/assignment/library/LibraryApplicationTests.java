6e17e75da8fd1f994ae2e9ef782d8d732c650431
YunSeok-Choi <dbstjr9898@naver.com>
2025-02-13 23:05:20 +0900

    :white_check_mark: Test : 빌드 성공 테스

--- library/src/test/java/assignment/library/LibraryApplicationTests.java
+++ library/src/test/java/assignment/library/LibraryApplicationTests.java
@@ -1,13 +1,25 @@
 package assignment.library;
 
+import jakarta.persistence.PreUpdate;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = LibraryApplicationTests.class)
 class LibraryApplicationTests {
 
+	public int testValue;
+
+	@BeforeEach
+	void init() {
+		testValue = 1;
+	}
+
 	@Test
-	void contextLoads() {
+	void 테스트_확인() {
+		assertThat(testValue).isEqualTo(1);
 	}
 
 }
